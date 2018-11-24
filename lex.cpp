3618745592440472af298bdfdc495128446c261a
LAK132 <lakridesagain@gmail.com>
2018-11-24 13:33:06 +1030

    features

--- lex.cpp
+++ lex.cpp
@@ -38,6 +38,7 @@ namespace lak
     using std::unordered_map;
     using std::shared_ptr;
     using std::make_shared;
+    using std::ostream;
 
     template<typename T>
     struct suffix_trie_t
@@ -53,16 +54,28 @@ namespace lak
         suffix_trie_t(const string str, const vector<T> &val) : key(str), values(val) {}
         suffix_trie_t(const string str, vector<T> &&val) : key(str), values(val) {}
 
-        inline shared_ptr<suffix_trie_t<T>> find(const string str) const
+        inline shared_ptr<suffix_trie_t<T>> find_partial(const char c) const
         {
-            if (auto &&it = children.find(str[0]); it != children.end())
+            if (auto &&it = children.find(c); it != children.end())
+                return it->second;
+            return nullptr;
+        }
+
+        inline shared_ptr<suffix_trie_t<T>> operator[](const char c) const
+        {
+            return find_partial(c);
+        }
+
+        inline shared_ptr<suffix_trie_t<T>> find_exact(const string str) const
+        {
+            if (auto &&it = children.find(str[0]); it != children.end() && it->second->key == str)
                 return it->second;
             return nullptr;
         }
 
         inline shared_ptr<suffix_trie_t<T>> operator[](const string str) const
         {
-            return find(str);
+            return find_exact(str);
         }
 
         inline bool isTerminal() { return !children.size(); }
@@ -70,31 +83,31 @@ namespace lak
         void set(const string str, vector<T> &&val) { set(str, val); }
         void set(const string str, const vector<T> &val)
         {
-            if (auto &it = children.find(str[0]); it != children.end() && it->second != nullptr)
+            if (auto &&it = find_partial(str[0]); it != nullptr)
             {
-                string &k = it->second->key;
+                string &k = it->key;
                 if (str.size() >= k.size() && !std::strncmp(str.c_str(), k.c_str(), k.size()))
                 {
                     if (str.size() == k.size())
                     {
                         // they are the same
-                        it->second->values = val;
+                        it->values = val;
                     }
                     else
                     {
                         // k is the suffix of str
-                        it->second->set(str.substr(k.size()), val);
+                        it->set(str.substr(k.size()), val);
                     }
                 }
                 else
                 {
                     // key slot is taken, but this str doesn't match current slot key.
                     // must split slot into largest common substring
-                    size_t same = 0;
+                    size_t same = 1; // start at 1 because we know the first character is the same
                     for (; same < str.size() && same < k.size() && str[same] == k[same]; ++same);
 
-                    string commonstr = str.substr(0, same); // part of the string that's the same
-                    string oldstr = k.substr(same);         // rest of the old key string
+                    string &&commonstr = str.substr(0, same); // part of the string that's the same
+                    string &&oldstr = k.substr(same);         // rest of the old key string
                     shared_ptr<suffix_trie_t<T>> replacement;
 
                     if (same == str.size())
@@ -107,26 +120,41 @@ namespace lak
                     {
                         // str and k have different endings
                         // therefore we must add the new node and the replacement node seperately
-                        string setstr = str.substr(same);       // rest of the set key string
                         replacement = make_shared<suffix_trie_t<T>>(commonstr);
-                        shared_ptr<suffix_trie_t<T>> set = make_shared<suffix_trie_t<T>>(setstr, val);
-                        replacement->children[setstr[0]] = set;
+                        string &&setstr = str.substr(same);   // rest of the set key string
+                        replacement->children[setstr[0]] = make_shared<suffix_trie_t<T>>(setstr, val);
                     }
 
-                    shared_ptr<suffix_trie_t<T>> old = it->second;
-                    old->key = oldstr;
-                    replacement->children[oldstr[0]] = old;
+                    it->key = oldstr;
+                    replacement->children[oldstr[0]] = it;
 
                     children[commonstr[0]] = replacement;
                 }
             }
             else
             {
-                children[str] = make_shared<suffix_trie_t<T>>(val);
+                children[str[0]] = make_shared<suffix_trie_t<T>>(str, val);
             }
+        }
+
+        friend ostream &operator<<(ostream &strm, const suffix_trie_t &rhs)
+        {
+            static size_t offset = 0;
+            string space = "";
+            for (size_t i = 0; i < offset; ++i)
+                space += ' ';
+
+            offset += 2;
+            for (const auto &[childk, childv] : rhs.children)
+                strm << '\n' << space << "child "  << childv->key << " " << *childv;
+            offset -= 2;
+            return strm;
         }
     };
 }
+
+
+
 
 namespace lex
 {
@@ -176,10 +204,11 @@ namespace lex
         while (strm.good() && isspace((char)strm.peek(), loc)) strm.get(); // skip whitespace
         for (char p = 0, c = strm.get(); strm.good(); c = strm.get())
         {
-            if (hit_word_boundry(p, c))
+            auto &&it = tokens.find_exact(str);
+            if (hit_word_boundry(p, c) || (it != nullptr && it->isTerminal() && it->values.size() > 0 && it->values[0] == token_type::SYMBOL))
             {
                 // reached the end of the token
-                if (auto &&it = tokens.find(str); it != nullptr)
+                if (it != nullptr && it->values.size() > 0)
                 {
                     // we found a token!
                     rtn.type = it->values[0];
@@ -206,16 +235,40 @@ namespace lex
 using std::ifstream;
 using std::cout;
 using std::cin;
+using std::vector;
 using std::string;
+using namespace std::string_literals;
 
 int main()
 {
+    vector<string> symbols = {
+        "~","~=","`","!","!=","@","#","$","%","%=","^","^=","&","&=","&&","*","*=","-","-=","+","+=","=",
+        "(",")","[","}","[","]","|","|=","||",":","<","<=",">",">=","==",",",".","?","/","'","->","\\"
+    };
+    for (const string &str : symbols)
+        lex::tokens.set(str, {lex::token_type::SYMBOL});
+    vector<string> keywords = {
+        "for", "while", "if", "switch", "case", "default", "break", "const", "constexpr", "return",
+        "friend", "public", "private", "protected", "struct", "enum", "union", "class"
+    };
+    for (const string &str : keywords)
+        lex::tokens.set(str, {lex::token_type::KEYWORD});
+
+    cout << lex::tokens;
+
     string filename;
     std::getline(cin, filename);
     if (ifstream strm(filename, ifstream::in | ifstream::binary); strm.is_open())
     {
         for (lex::token_t t = lex::next_token(strm); t.type != lex::token_type::END; t = lex::next_token(strm))
         {
+            switch(t.type)
+            {
+                case lex::token_type::END: { cout << "END: "; } break;
+                case lex::token_type::USER: { cout << "USER: "; } break;
+                case lex::token_type::KEYWORD: { cout << "KEYWORD: "; } break;
+                case lex::token_type::SYMBOL: { cout << "SYMBOL: "; } break;
+            }
             cout << t.value << '\n';
         }
     }
